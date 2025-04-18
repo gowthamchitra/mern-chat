046c117a36e4dfc5c70e62f5879be8ec4e3b9e1d
gowthamchitra <gowthamkumarc9@gmail.com>
2025-04-18 17:04:33 +0530

    Update axiosConfig.js

--- client/src/components/profile-chat/axiosConfig.js
+++ client/src/components/profile-chat/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://ultra-chat-app.onrender.com",
+  baseURL: "https://mern-chat-backend-tdgo.onrender.com",
   withCredentials: true,
 });
 
