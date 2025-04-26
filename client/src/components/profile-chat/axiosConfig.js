720e9daabddd8c93d118b33c6ba9b8e483baf27e
gowthamchitra <gowthamkumarc9@gmail.com>
2025-04-26 21:49:30 +0530

    Update axiosConfig.js

--- client/src/components/profile-chat/axiosConfig.js
+++ client/src/components/profile-chat/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://mern-chat-backend-tdgo.onrender.com",
+  baseURL: "https://mern-chat-backend1-9zu0.onrender.com",
   withCredentials: true,
 });
 
